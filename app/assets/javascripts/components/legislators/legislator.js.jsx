bb97ee80174c7f8ab12f2e6a7a8627ad32e39003
Mitch Kroska <kromitj@gmail.com>
2017-02-02 17:26:35 -0700

    Get rid of collapsable table

--- app/assets/javascripts/components/legislators/legislator.js.jsx
+++ app/assets/javascripts/components/legislators/legislator.js.jsx
@@ -3,39 +3,23 @@ class Legislator extends React.Component {
     super();
     this.state = {
       legislator: props.legislator,
-      fields: ["first_name", "last_name", "state", "party", "id"],
-      imageURI: ""
+      fields: ["first_name", "last_name", "state", "party"]
     }
-    this.hangleChange = this.handleChange.bind( this )
   }
 
-   handleChange( value ) {;
-    console.log(value)
-    this.setState( { imageURI: "https://theunitedstates.io/images/congress/225x275/" + this.state.legislator.id + ".jpg" , legislator: this.props.legislator} ) 
+  handleChange( value ) {
+    console.log("inside handleCHange")
+    this.setState( { legislator: this.sort_by( value) })
   }
-
-
 
   render() {
-    console.log(this.state.imageURI)
      const legislatorRow = this.state.fields.map((field) =>
-      <LegislatorField value={ this.props.legislator[field] } />
+        <LegislatorField value={ this.props.legislator[field] } />
     )
     return (
-       <div className="card">
-        <div className="card-header" role="tab" id="headingOne">
-          <h5 className="mb-0">
-            <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne" onClick={ () => this.handleChange()}>
-              { legislatorRow}
-            </a>
-          </h5>
-        </div>
-        <div id="collapseOne" className="collapse" role="tabpanel" aria-labelledby="headingOne">
-          <div className="card-block">
-            <img src={this.state.imageURI} alt="..." className="img-thumbnail"></img>
-          </div>
-        </div>
-      </div>
+      <tr key={this.state.legislator.id}>
+        { legislatorRow }      
+      </tr>
     );
   }
 }
