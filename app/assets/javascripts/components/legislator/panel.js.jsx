040bf4f8a3bfc6c823f3386bdc27264a1ea994da
Mitch Kroska <kromitj@gmail.com>
2017-03-23 18:03:58 -0600

    Restructure role-show visual layout

---
+++ app/assets/javascripts/components/legislator/panel.js.jsx
@@ -0,0 +1,30 @@
+class Panel extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div className="panel panel-default target">
+		    <div className="panel-heading" >Recent News</div>
+		    <div className="panel-body">
+		      <div className="row">
+		        <div className="col-md-6">
+		          <div className="thumbnail">
+		            <div className="article-thumbnail">
+		            	
+		            </div>
+		            <div className="caption">
+		              
+		            </div>
+		          </div>
+		        </div>
+		      </div>
+		    </div>
+		  </div>      
+    )
+  }  
+}
--- app/assets/javascripts/components/legislator/role-article.js.jsx
+++ app/assets/javascripts/components/legislator/role-article.js.jsx
@@ -9,16 +9,18 @@ class RoleArticle extends React.Component {
   render() {
 
     return (
-              <div className="col-md-3">
-                <div className="thumbnail">
-                  <img src={this.props.article.thumbnail} alt="ALT NAME" className="img-responsive" ></img>
-                  <div className="caption">
-                       <h4>{this.props.article.title}</h4>
-                      <p>{this.props.article.snippet}</p>
-                      <p align="center"><a href={this.props.article.link} className="btn btn-primary btn-block">Open</a></p>
-                  </div>
-                </div>
-              </div>
+              <div className="thumbnail">
+                      <div className="article-thumbnail">
+                        <a href="http://www.cnn.com/2012/11/07/politics/wisconsin-tammy-baldwin-senate/" target="_blank"> 
+                        <img className="center-block" alt="300x200" width="300" src="http://i2.cdn.cnn.com/cnnnext/dam/assets/121103050436-stock-tammy-baldwin-story-top.jpg"></img>
+                        </a>
+                      </div>
+                      <div className="caption">
+                        <h4>Wisconsin&#39;s Tammy Baldwin is first openly gay person elected to ...</h4>
+                        <p>Nov 7, 2012 ... Tammy Baldwin made history Tuesday night -- twice. ... the first openly gay \npolitician, and first Wisconsin woman, elected to the U.S. Senate.</p>
+                        <p></p>
+                      </div>
+                    </div>
     )
   }
   
